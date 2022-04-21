9fe3cecfaa06df2a85e2ec7e5309c0d04c384630
GuHaoqinhomework <104123199+GuHaoqinhomework@users.noreply.github.com>
2022-04-21 13:54:04 +0800

    Add files via upload

--- mutable.py
+++ mutable.py
@@ -1,51 +1,48 @@
-import DynamicArray as DynamicArray
+import lab1.DynamicArray as DynamicArray
 import math
 import numpy as np
 from functools import reduce
 
-DynamicArray =DynamicArray.DynamicArray
-
+DynamicArray = DynamicArray.DynamicArray
 
 
 class fun_dynamicarray(DynamicArray):
-
-
 
     def __init__(self):
         """"Initialzes"""
         DynamicArray.__init__(self)
 
     # 1. add a new element
-    def insert(self, key,value):
-        DynamicArray.insert(self,key, value)
+    def insert(self, key, value):
+        DynamicArray.insert(self, key, value)
 
     # 2. remove an element
     def deleteindex(self, key):
-        if (self._len_()<key):
+        if (self._len_() < key):
             print("key is out of range")
             return
         self.pop(key)
 
-    def deletevalue(self,value):
+    def deletevalue(self, value):
         self.remove(value)
 
     # 3. size
     def size(self):
         return (self._len_())
 
-    #4.ismember
-    def ismember(self,value):
+    # 4.ismember
+    def ismember(self, value):
         for k in range(self.n):
             if self.A[k] == value:
                 return True
         return False
 
-    #5.reverse
+    # 5.reverse
     def reverse(self):
-        for i in range(int(self.size()/2)):
-            num=self.A[i]
-            self.A[i]=self.A[self.size()-i-1]
-            self.A[self.size()-i-1]=num
+        for i in range(int(self.size() / 2)):
+            num = self.A[i]
+            self.A[i] = self.A[self.size() - i - 1]
+            self.A[self.size() - i - 1] = num
         return self
 
     # 6. conversion from and to python lists
@@ -54,16 +51,16 @@ class fun_dynamicarray(DynamicArray):
             self.insert(i, list[i])
 
     def to_list(self):
-        ls={}
+        ls = {}
         for i in range(self.size()):
-            ls[i]=self.A[i]
+            ls[i] = self.A[i]
         return ls
 
     # 7. find element by specific predicate
     def find_value(self, key):
         if key >= self.n or key < 0:
             raise ValueError('invalid index')
-        return self.A[key];
+        return self.A[key]
 
     def find_key(self, value):
         for k in range(self.n):
@@ -72,25 +69,25 @@ class fun_dynamicarray(DynamicArray):
         return False
 
     # 8. filter data structure by specific predicate
-    def value_is_odd(self,n):
+    def value_is_odd(self, n):
         return n % 2 == 1
 
-    def value_is_even(self,n):
+    def value_is_even(self, n):
         return n % 2 == 0
 
-    def is_sqr(self,x):
+    def is_sqr(self, x):
         return math.sqrt(x) % 1 == 0
 
     def filter_func(self, fun):
-        arr=np.zeros((self.size()))
+        arr = np.zeros((self.size()))
         for i in range(self.size()):
-            arr[i]=self.A[i]
+            arr[i] = self.A[i]
         newlist = filter(fun, arr)
         for i in newlist:
             self.deletevalue(i)
 
     # 9. map structure by specific function
-    def square(self,x):
+    def square(self, x):
         return x ** 2
 
     def map_func(self, fun):
@@ -98,16 +95,17 @@ class fun_dynamicarray(DynamicArray):
         for i in range(self.size()):
             arr[i] = self.A[i]
         newlist = map(fun, arr)
-        func= fun_dynamicarray()
-        j=0;
+        func = fun_dynamicarray()
+        j = 0
         for i in newlist:
             func.insert(j, i)
             j = j + 1
         return func
 
-    # 10. reduce – process structure elements to build a return value by specific functions
+    # 10. reduce – process structure elements to build a return value by
+    # specific functions
     def add(self, x, y):
-        return x+y
+        return x + y
 
     def reduce_func(self, func):
         arr = np.zeros((self.size()))
@@ -118,30 +116,29 @@ class fun_dynamicarray(DynamicArray):
 
     # 11. iterator
     def __iter__(self):
-        self.num=-1
+        self.num = -1
         return self
 
     def next(self):
-        self.num=self.num+1
+        self.num = self.num + 1
         return self.A[self.num]
 
     # 12. mempty and mconcat
     def mempty(self):
-        self=None
+        self = None
         return self
 
     def mconcat(self, a):
-        if self == None:
+        if self is None:
             return a
-        elif a == None:
+        elif a is None:
             return self
         else:
             arr = np.zeros((a.size()))
             for i in range(a.size()):
                 arr[i] = a.A[i]
-            j=self.size()
+            j = self.size()
             for i in arr:
                 self.insert(j, i)
-                j=j+1
+                j = j + 1
             return self
-
--- mutable_test.py
+++ mutable_test.py
@@ -1,22 +1,19 @@
 import unittest
-from mutable import fun_dynamicarray as fun_dynamicarray
-import DynamicArray as DynamicArray
+from lab1.mutable import fun_dynamicarray as fun_dynamicarray
+import lab1.DynamicArray as DynamicArray
 from functools import reduce
 import numpy as np
 
 
-
 class Testfunction(unittest.TestCase):
 
-
-
     def test_add(self):
-        func =fun_dynamicarray()
+        func = fun_dynamicarray()
         func.insert(0, 1)
         func.insert(1, 4)
         func.insert(2, 5)
         func.insert(3, 3)
-        #display(func)
+        # display(func)
 
     def test_delete(self):
         func = fun_dynamicarray()
@@ -24,11 +21,11 @@ class Testfunction(unittest.TestCase):
         func.insert(1, 4)
         func.insert(2, 5)
         func.insert(3, 3)
-        #display(func)
+        # display(func)
         func.deleteindex(2)
-        #display(func)
+        # display(func)
         func.deletevalue(1)
-        #display(func)
+        # display(func)
 
     def test_size(self):
         func = fun_dynamicarray()
@@ -36,7 +33,7 @@ class Testfunction(unittest.TestCase):
         func.insert(1, 4)
         func.insert(2, 5)
         func.insert(3, 3)
-        #print(func.size())
+        # print(func.size())
 
     def test_reverse(self):
         func = fun_dynamicarray()
@@ -44,16 +41,16 @@ class Testfunction(unittest.TestCase):
         func.insert(1, 4)
         func.insert(2, 5)
         func.insert(3, 3)
-        func1=func.reverse()
-        #display(func1)
+        func1 = func.reverse()
+        # display(func1)
 
     def test_from_list(self):
-        list = [5,6]
+        list = [5, 6]
         func = fun_dynamicarray()
         func.from_list(list)
         self.assertEqual(func.A[0], 5)
         self.assertEqual(func.A[1], 6)
-        #display(func)
+        # display(func)
 
     def test_to_list(self):
         func = fun_dynamicarray()
@@ -61,12 +58,12 @@ class Testfunction(unittest.TestCase):
         func.insert(1, 4)
         func.insert(2, 5)
         func.insert(3, 3)
-        ls=func.to_list()
-        ls1={}
+        ls = func.to_list()
+        ls1 = {}
         for i in range(func.size()):
-            ls1[i]=func.A[i]
+            ls1[i] = func.A[i]
         self.assertEqual(ls1, ls)
-        #display(func)
+        # display(func)
 
     def test_find(self):
         func = fun_dynamicarray()
@@ -74,11 +71,11 @@ class Testfunction(unittest.TestCase):
         func.insert(1, 4)
         func.insert(2, 5)
         func.insert(3, 3)
-        x=func.find_value(3)
-        y=func.find_key(3)
+        x = func.find_value(3)
+        y = func.find_key(3)
         self.assertEqual(x, y)
-        x1= func.find_value(1)
-        y1= func.A[1]
+        x1 = func.find_value(1)
+        y1 = func.A[1]
         self.assertEqual(x1, y1)
 
     def test_filter_func(self):
@@ -87,10 +84,10 @@ class Testfunction(unittest.TestCase):
         func.insert(1, 4)
         func.insert(2, 5)
         func.insert(3, 3)
-        #func.filter_func(func.value_is_odd)
-        #func.filter_func(func.value_is_even)
+        # func.filter_func(func.value_is_odd)
+        # func.filter_func(func.value_is_even)
         func.filter_func(func.is_sqr)
-        #display(func)
+        # display(func)
 
     def test_map_func(self):
         func = fun_dynamicarray()
@@ -99,7 +96,7 @@ class Testfunction(unittest.TestCase):
         func.insert(2, 5)
         func.insert(3, 3)
         func1 = func.map_func(func.square)
-        #display(func1)
+        # display(func1)
 
     def test_reduce_func(self):
         func = fun_dynamicarray()
@@ -108,7 +105,7 @@ class Testfunction(unittest.TestCase):
         func.insert(2, 5)
         func.insert(3, 3)
         sum = func.reduce_func(func.add)
-        #print(sum)
+        # print(sum)
 
     def test_iter(self):
         func = fun_dynamicarray()
@@ -117,10 +114,10 @@ class Testfunction(unittest.TestCase):
         func.insert(2, 5)
         func.insert(3, 3)
         func.__iter__()
-        #print(func.next())
-        #print(func.next())
-        #print(func.next())
-        #print(func.next())
+        # print(func.next())
+        # print(func.next())
+        # print(func.next())
+        # print(func.next())
 
     def test_mempty(self):
         func = fun_dynamicarray()
@@ -136,13 +133,14 @@ class Testfunction(unittest.TestCase):
         func.insert(1, 4)
         func.insert(2, 5)
         func.insert(3, 3)
-        #display(func)
+        # display(func)
         func1 = fun_dynamicarray()
         func1.insert(0, 6)
         func1.insert(1, 7)
-        #display(func1)
+        # display(func1)
         func = func.mconcat(func1)
-        #display(func)
+        # display(func)
+
 
 def display(self):
     if not DynamicArray:
@@ -150,5 +148,6 @@ def display(self):
     for i in range(self.size()):
         print(self.A[i])
 
+
 if __name__ == '__main__':
-        unittest.main()
+    unittest.main()
