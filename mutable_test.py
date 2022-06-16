8d953fdc7de8971ae07e1daee7ca5ccb2118b94b
GuHaoqinhomework <104123199+GuHaoqinhomework@users.noreply.github.com>
2022-06-16 10:17:21 +0800

    Update mutable_test.py

--- mutable_test.py
+++ mutable_test.py
@@ -1,153 +1,86 @@
-# name
 import unittest
-from mutable import fun_dynamicarray as fun_dynamicarray
-import DynamicArray as DynamicArray
-from functools import reduce
-import numpy as np
+from discrete_event import *
+import math
 
+class SexpTest(unittest.TestCase):
 
-class Testfunction(unittest.TestCase):
+    def test_parse(self):
+        e = Sexp()
+        program = "(begin (define a 10) (* a 3))"
+        self.assertEqual(e.parse(program), ['begin', ['define', 'a', 10], ['*', 'a', '3']])
 
-    def test_add(self):
-        func = fun_dynamicarray()
-        func.insert(0, 1)
-        func.insert(1, 4)
-        func.insert(2, 5)
-        func.insert(3, 3)
-        # display(func)
+    def test_tokenize(self):
+        e = Sexp()
+        program = "(begin (define a 10) (* a 3))"
+        self.assertEqual(e.tokenize(program), ['(', 'begin', '(', 'define', 'a', '10', ')', '(', '*', 'a', '3', ')', ')'])
 
-    def test_delete(self):
-        func = fun_dynamicarray()
-        func.insert(0, 1)
-        func.insert(1, 4)
-        func.insert(2, 5)
-        func.insert(3, 3)
-        # display(func)
-        func.deleteindex(2)
-        # display(func)
-        func.deletevalue(1)
-        # display(func)
+    def test_read_from_tokens(self):
+        exp = Sexp()
+        self.assertRaises(SyntaxError, lambda: exp.parse(''))
+        try: exp.read_from_tokens(['(', 'print', 'a', '10', ')', ')'])
+        except SyntaxError as e:
+            self.assertEqual(e.args[0], 'unexpected )')
+        program = ['(', 'define', 'a', '10', ')']
+        self.assertEqual(exp.read_from_tokens(program), ['define', 'a', 10])
 
-    def test_size(self):
-        func = fun_dynamicarray()
-        func.insert(0, 1)
-        func.insert(1, 4)
-        func.insert(2, 5)
-        func.insert(3, 3)
-        # print(func.size())
+    def test_atom(self):
+        exp = Sexp()
+        self.assertEqual(exp.atom('6'), 6)
+        self.assertEqual(exp.atom('4.1'), 4.1)
+        self.assertEqual(exp.atom('+'), '+')
+        self.assertEqual(exp.atom('and'), 'and')
 
-    def test_reverse(self):
-        func = fun_dynamicarray()
-        func.insert(0, 1)
-        func.insert(1, 4)
-        func.insert(2, 5)
-        func.insert(3, 3)
-        func1 = func.reverse()
-        # display(func1)
+    def test_standard_env(self):
+        env = Env()
+        env.update(vars(math))  # sin, cos, sqrt, pi, ...
+        env.update({
+            '+': op.add, '-': op.sub, '*': op.mul, '/': op.truediv,
+            '>': op.gt, '<': op.lt, '>=': op.ge, '<=': op.le, '=': op.eq,
+            'print': print,
+            'and': op.and_,
+            'or': op.or_,
+            'not': op.not_,
+        })
+        print(env)
+        self.assertEqual(Sexp().standard_env(), env)
 
-    def test_from_list(self):
-        list = [5, 6]
-        func = fun_dynamicarray()
-        func.from_list(list)
-        self.assertEqual(func.A[0], 5)
-        self.assertEqual(func.A[1], 6)
-        # display(func)
+    def test_eval(self):
+        exp = Sexp()
+        # test 'print'
+        exp.eval(exp.parse('(print r 5)'))
+        exp.eval(exp.parse('(print r1 2)'))
+        exp.eval(exp.parse('(print r2 1)'))
+        # test '+' '-' '*' '/' 'sin' 'pi'
+        self.assertEqual(exp.eval(exp.parse('(+ r (- 2 (* (sin -0.3) (- (* pi (* r r)) (/ r1 r2)))))')),
+                         5 + 2 - math.sin(-0.3) * (314.1592653589793-2/1))
+        # test '=' '>' '<' 'if'
+        self.assertEqual(exp.eval(exp.parse('(if (> (* 11 11) 120) (* 7 6) (= r 5))')), 42)
+        self.assertEqual(exp.eval(exp.parse('(if (< (* 11 11) 120) (* 7 6) (= r 5))')), True)
+        # test 'and' 'or' 'not'
+        self.assertEqual(exp.eval(exp.parse('(and 1 0)')), 0)
+        self.assertEqual(exp.eval(exp.parse('(or 1 0)')), 1)
+        self.assertEqual(exp.eval(exp.parse('(not 1)')), 0)
 
-    def test_to_list(self):
-        func = fun_dynamicarray()
-        func.insert(0, 1)
-        func.insert(1, 4)
-        func.insert(2, 5)
-        func.insert(3, 3)
-        ls = func.to_list()
-        ls1 = {}
-        for i in range(func.size()):
-            ls1[i] = func.A[i]
-        self.assertEqual(ls1, ls)
-        # display(func)
-
+class EnvTest(unittest.TestCase):
     def test_find(self):
-        func = fun_dynamicarray()
-        func.insert(0, 1)
-        func.insert(1, 4)
-        func.insert(2, 5)
-        func.insert(3, 3)
-        x = func.find_value(3)
-        y = func.find_key(3)
-        self.assertEqual(x, y)
-        x1 = func.find_value(1)
-        y1 = func.A[1]
-        self.assertEqual(x1, y1)
+        e = Env()
+        dict = {'+': op.add, '-': op.sub, '*': op.mul, '/': op.truediv}
+        e.update(dict)
+        self.assertEqual(e.find('-'), e)
+        try:  e.find('>')
+        except AttributeError as e:
+            self.assertEqual(e.args[0], "This arithmetic symbol does not exist")
 
-    def test_filter_func(self):
-        func = fun_dynamicarray()
-        func.insert(0, 1)
-        func.insert(1, 4)
-        func.insert(2, 5)
-        func.insert(3, 3)
-        # func.filter_func(func.value_is_odd)
-        # func.filter_func(func.value_is_even)
-        func.filter_func(func.is_sqr)
-        # display(func)
-
-    def test_map_func(self):
-        func = fun_dynamicarray()
-        func.insert(0, 1)
-        func.insert(1, 4)
-        func.insert(2, 5)
-        func.insert(3, 3)
-        func1 = func.map_func(func.square)
-        # display(func1)
-
-    def test_reduce_func(self):
-        func = fun_dynamicarray()
-        func.insert(0, 1)
-        func.insert(1, 4)
-        func.insert(2, 5)
-        func.insert(3, 3)
-        sum = func.reduce_func(func.add)
-        # print(sum)
-
-    def test_iter(self):
-        func = fun_dynamicarray()
-        func.insert(0, 1)
-        func.insert(1, 4)
-        func.insert(2, 5)
-        func.insert(3, 3)
-        func.__iter__()
-        # print(func.next())
-        # print(func.next())
-        # print(func.next())
-        # print(func.next())
-
-    def test_mempty(self):
-        func = fun_dynamicarray()
-        func.insert(0, 1)
-        func.insert(1, 4)
-        func.insert(2, 5)
-        func.insert(3, 3)
-        # self.assertEqual(func.mempty(), None)
-
-    def test_mconcat(self):
-        func = fun_dynamicarray()
-        func.insert(0, 1)
-        func.insert(1, 4)
-        func.insert(2, 5)
-        func.insert(3, 3)
-        # display(func)
-        func1 = fun_dynamicarray()
-        func1.insert(0, 6)
-        func1.insert(1, 7)
-        # display(func1)
-        func = func.mconcat(func1)
-        # display(func)
-
-
-def display(self):
-    if not DynamicArray:
-        return
-    for i in range(self.size()):
-        print(self.A[i])
+class test_Procedure(unittest.TestCase):
+    def test(self):
+        exp = Sexp()
+        exp.eval(exp.parse('(define twice (lambda (x) (* 2 x)))'))
+        self.assertEqual(exp.eval(exp.parse('(twice 5)')), 10)
+        exp.eval(exp.parse('(define repeat (lambda (f) (lambda (x) (f (f x)))))'))
+        self.assertEqual(exp.eval(exp.parse('((repeat twice) 10)')), 40)
+        self.assertEqual(exp.eval(exp.parse('((repeat (repeat twice)) 10)')), 160)
+        self.assertEqual(exp.eval(exp.parse('((repeat (repeat (repeat twice))) 10)')), 2560)
+        self.assertEqual(exp.eval(exp.parse('((repeat (repeat (repeat (repeat twice)))) 10)')), 655360)
 
 
 if __name__ == '__main__':
